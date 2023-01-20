6c742c7600d39bfdc4967f421c61c1ca3fa42299
ficapp2.0 <ficapp2.0@gmail.com>
2023-01-20 19:39:40 +0100

    feat: content

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
-
+const hbs = require('hbs')
 const app = express()
 
 app.set('view engine', 'hbs')
 app.set('views', `${__dirname}/views`)
+hbs.registerPartials(`${__dirname}/views/partials`)
+app.use(express.static(`${__dirname}/public`))
 
 app.get('/', (req, res, next)=> {
   res.render('home')
